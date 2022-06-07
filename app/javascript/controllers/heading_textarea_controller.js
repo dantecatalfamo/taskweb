493914f7b8e4d2026612fb658b8e06937c6e18ef
dantecatalfamo <dante.catalfamo@gmail.com>
2022-06-07 00:05:23 -0400

    Handle bullet points and indentation in text area

---
+++ app/javascript/controllers/heading_textarea_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+  }
+
+  processInput(event) {
+    if (event.keyCode === 13) {
+      // If the previous line begins with a bullet point or
+      // indentation, add it to the beginning of this line
+      const pos = this.element.selectionStart;
+      const newlines = this.newlinePositions();
+      const prevLineBegin = newlines.reduce((acc, n) => (n < pos-1 ? n : acc));
+      const prevLine = this.element.value.substring(prevLineBegin+1, pos).replace('\n', '');
+      const lead = prevLine.match(/^\W*/)[0];
+      this.element.setRangeText(lead);
+      this.element.selectionStart += lead.length;
+    }
+  }
+
+  newlinePositions() {
+    const len = this.element.value.length;
+    const positions = [];
+    for (let i = 0; i < len; i++) {
+      if (this.element.value[i] === '\n') {
+        positions.push(i);
+      }
+    }
+    return positions;
+  }
+}
