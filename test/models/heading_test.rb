318dd9190bbec674600dc7f7e4735c794abd216e
dantecatalfamo <dante.catalfamo@gmail.com>
2022-06-20 01:10:37 -0400

    Test for closed_at

--- test/models/heading_test.rb
+++ test/models/heading_test.rb
@@ -9,4 +9,22 @@ class HeadingTest < ActiveSupport::TestCase
   test 'should create empty heading' do
     assert Heading.new(notebook_id: notebooks(:one).id).save
   end
+
+  test 'should set closed_at properly' do
+    heading = Heading.new(notebook_id: notebooks(:one).id, heading_state_id: heading_states(:todo))
+    heading.save!
+    assert_not heading.closed_at
+    heading.state = heading_states(:done)
+    heading.save!
+    assert heading.closed_at
+    heading.state = heading_states(:todo)
+    heading.save!
+    assert_not heading.closed_at
+    heading.state = nil
+    heading.save!
+    assert_not heading.closed_at
+    heading.state = heading_states(:done)
+    heading.save!
+    assert heading.closed_at
+  end
 end
