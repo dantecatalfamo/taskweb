df4b4a29a18ffa0c3247e54dcf68875215b6f8a8
Dante Catalfamo <dante.catalfamo@gmail.com>
2022-06-21 12:56:25 -0400

    Add home controller tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,24 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get home_url
     assert_response :success
   end
+
+  test 'should show notebooks' do
+    get home_url
+
+    assert_select '#notebooks' do
+      assert_select "\##{dom_id notebooks(:one)}" do
+        assert_select 'a', notebooks(:one).title
+      end
+    end
+  end
+
+  test 'should show agenda' do
+    get home_url
+
+    assert_select '#agenda' do
+      assert_select '#agenda-dates' do
+        assert_select 'li span', HeadingsController.helpers.agenda_date(Date.today)
+      end
+    end
+  end
 end
