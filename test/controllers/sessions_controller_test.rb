39e6fcf04ed17fc563de5a095cd4cb34b2c2f88c
dantecatalfamo <dante.catalfamo@gmail.com>
2022-09-15 22:36:50 -0400

    Add login and logout tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test 'user login' do
+    get notebooks_path
+    assert_response :redirect
+    post login_url, params: { login: { username: users(:one).username, password: 'secret' } }
+    get notebooks_path
+    assert_response :success
+  end
+
+  test 'user logout' do
+    post login_url, params: { login: { username: users(:one).username, password: 'secret' } }
+    get notebooks_path
+    assert_response :success
+    delete logout_path
+    get notebooks_path
+    assert_response :redirect
+  end
 end
