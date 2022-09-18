26415023efaa8caa832805b75b0ec789b4fb98d0
dantecatalfamo <dante.catalfamo@gmail.com>
2022-09-18 00:42:29 -0400

    Add login/logout logging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,15 @@
 class SessionsController < ApplicationController
   def create
+    Rails.logger.info("Attempting to login as #{session_params[:username]}")
     @user = User.find_by(username: session_params[:username])
     if @user&.authenticate(session_params[:password])
+      Rails.logger.info("Successful login as #{session_params[:username]}")
       reset_session
       session[:user_id] = @user.id
       flash[:notice] = 'logged in'
       redirect_to home_path
     else
+      Rails.logger.info("Failed login as #{session_params[:username]}")
       flash.now[:alert] = 'Incorrect email or password'
       render :new, status: :unprocessable_entity
     end
@@ -15,6 +18,7 @@ class SessionsController < ApplicationController
   def new; end
 
   def destroy
+    Rails.logger.info("Logout from #{current_user.username}")
     reset_session
     redirect_to home_path, notice: 'Signed out'
   end
