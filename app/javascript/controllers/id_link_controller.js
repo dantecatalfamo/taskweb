651904a4c74f71bf5c4dbdffbb98a53594bf9723
Dante Catalfamo <dante.catalfamo@gmail.com>
2022-09-27 00:08:46 -0400

    Add better copy-org-id-link JS

---
+++ app/javascript/controllers/id_link_controller.js
@@ -0,0 +1,32 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["icon"];
+  static values = {
+    id: String,
+    title: String,
+  }
+
+  chainIcon = 'bi-link-45deg';
+  checkIcon = 'bi-check';
+
+  connect() {
+  }
+
+  copy(e) {
+    e.preventDefault();
+    this.check();
+    window.setTimeout(() => this.uncheck(), 2000);
+    navigator.clipboard.writeText(`[[id:${this.idValue}][${this.titleValue}]]`);
+  }
+
+  check() {
+    this.iconTarget.classList.remove(this.chainIcon);
+    this.iconTarget.classList.add(this.checkIcon);
+  }
+
+  uncheck() {
+    this.iconTarget.classList.remove(this.checkIcon);
+    this.iconTarget.classList.add(this.chainIcon);
+  }
+}
