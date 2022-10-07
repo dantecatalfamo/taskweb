20826283f41c52c3dbfc6e1dba8e58359d310cb1
dantecatalfamo <dante.catalfamo@gmail.com>
2022-10-07 15:02:54 -0400

    Remove complex main nav turbo frame with turbo drive

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -6,9 +6,6 @@ export default class extends Controller {
   connect() {
     this.headingsTarget.addEventListener("turbo:submit-end", this.reloadAgenda);
     this.agendaRefresh = setInterval(this.reloadAgenda, 5 * 60 * 1000);
-    const notebooks = this.headingsTarget.querySelector("#notebooks");
-    notebooks.dataset.turboAction = 'advance';
-    window.notebooks = notebooks;
   }
 
   disconnect() {
