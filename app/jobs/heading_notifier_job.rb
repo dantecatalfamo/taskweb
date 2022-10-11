f9bc4179271b616afcef822a3ecaef140d139518
dantecatalfamo <dante.catalfamo@gmail.com>
2022-10-11 01:12:25 -0400

    Don't pick up headings marked as done in notification job

--- app/jobs/heading_notifier_job.rb
+++ app/jobs/heading_notifier_job.rb
@@ -2,7 +2,7 @@ class HeadingNotifierJob < ApplicationJob
   queue_as :default
 
   def perform
-    headings = Heading.dates_between(Time.now, 10.minutes.from_now)
+    headings = Heading.not_todo_or_done.dates_between(Time.now, 10.minutes.from_now)
     headings.each { |heading| PushoverNotifyJob.perform_later(heading) }
   end
 end
